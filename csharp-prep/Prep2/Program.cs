269d7419c3c9a63411916ee6afb1b783b80522da
Thomas Agbese <agb23010@byui.edu>
2024-03-02 18:06:14 +0100

    week 1 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,70 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage?: ");
+        string inputFromUser = Console.ReadLine();
+        int percentage = int.Parse(inputFromUser);
+
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        if ((percentage % 10) >= 7)
+        {
+            sign = "+";
+        }
+        else if ((percentage % 10) < 3)
+        {
+            sign = "-";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            letter = "A";
+            sign = "-";
+        }
+        else if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!!!");
+        }
+
+        else 
+        {
+            Console.WriteLine("You can do better, try again.");
+        }
+
+    
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+        
+        while (playAgain)
+        {
+            // Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number?: ");
+        
+        Random randomNumberGenerator = new Random();
+        int magicNumber = randomNumberGenerator.Next(1, 101);
+
+        int guess = -1;
+        int numberOfGuesses = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numberOfGuesses++;
+
+            if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else 
+        {
+            Console.WriteLine($"Great, you guessed it in {numberOfGuesses} guesses!");
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainResponse = Console.ReadLine().ToLower();
+
+            if (playAgainResponse != "yes")
+            {
+                playAgain = false;
+            }
+
+            Console.WriteLine("Thanks for playing");
+        }
+
+        
+        }
+        }
+
+        
+
     }
 }
